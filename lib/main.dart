8ec73d0351beddddfeeb3beae95f5fb77e818a6d
MatheusConaga <matheusphillip170@gmail.com>
2024-10-03 11:44:08 -0300

    Crud no firestore do firebase

--- lib/main.dart
+++ lib/main.dart
@@ -6,8 +6,25 @@ void main() async{
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
 
-  FirebaseFirestore.instance.collection("usuarios").doc("pontuacao").set(
-      {"Matheus": "250", "Joao": "300"});
+  FirebaseFirestore db = FirebaseFirestore.instance;
+  
+  QuerySnapshot querySnapshot = await db.collection("usuarios")
+  // .where("idade", isEqualTo: "21")
+  // .where("idade", isGreaterThan: 20)
+  // .where("idade", isLessThan: 40)
+  .orderBy("idade", descending: true)
+  .orderBy("nome", descending: false)
+  .limit(3)
+  .get();
+
+  for( DocumentSnapshot item in querySnapshot.docs){
+
+    var dados = item;
+    print("filtro nome: ${dados["nome"]} - idade: ${dados["idade"]}");
+
+  }
+
+
 
   runApp(MaterialApp(
     home: MyApp(),
