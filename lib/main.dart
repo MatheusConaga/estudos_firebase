cc794b54cbe6ba34b3167b3ac8336c5086027e78
MatheusConaga <matheusphillip170@gmail.com>
2024-10-03 10:21:44 -0300

    configuração inicial do firebase e primeira instancia no banco de dados

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
+  FirebaseFirestore.instance.collection("usuarios").doc("pontuacao").set(
+      {"Matheus": "250", "Joao": "300"});
+
+  runApp(MaterialApp(
+    home: MyApp(),
+  ));
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
